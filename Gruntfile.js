51a5f43094300ad24c7e0fa92a65e13ec6b02b0a
gaddam1987 <gaddam.1987@gmail.com>
2013-07-17 10:16:56 +0200

    basic pom angular javascript project first commit

---
+++ Gruntfile.js
@@ -0,0 +1,257 @@
+module.exports = function (grunt) {
+    'use strict';
+    var LIVERELOAD_PORT = 35729;
+    var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+    var mountFolder = function (connect, dir) {
+        return connect.static(require('path').resolve(dir));
+    };
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    var config = {
+        app: 'src',
+        dist: 'target'
+    };
+
+    grunt.initConfig({
+        config: config,
+        watch: {
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    '<%= config.dest%>/main/resources/templates/**/*.html',
+                    '<%= config.app%>/main/index.html',
+                    '{.tmp,<%= config.app %>}/main/resources/styles/**/*.css',
+                    '{.tmp,<%= config.app %>}/main/js/**/*.js',
+                    '!{.tmp,<%= config.app %>}/main/js/lib/**/*.js',
+                    '<%= config.app %>/images/main/resources/**/*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, config.app)
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, '<%= config.app %>/test')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, config.dist)
+                        ];
+                    }
+                }
+            },
+            open: {
+                server: {
+                    url: 'http://localhost:<%= connect.options.port %>'
+                }
+            }
+        },
+        clean: {
+            dist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            '.tmp',
+                            '<%= config.dist %>/*',
+                            '!<%= config.dist %>/.git*'
+                        ]
+                    }
+                ]
+            },
+            server: '.tmp'
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= config.app %>/main/js/**/*.js',
+                '!<%= config.app %>/main/js/lib/**/*.js'
+            ]
+        },
+        useminPrepare: {
+            html: '<%= config.app %>/main/index.html',
+            options: {
+                dest: '<%= config.dist %>'
+            }
+        },
+        usemin: {
+            html: ['<%= config.dist %>/templates/**/.html','<%= config.dist %>/*.html'],
+            css: ['<%= config.dist %>/styles/**/*.css'],
+            options: {
+                dirs: ['<%= config.dist %>']
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= config.app %>/main/resources/images',
+                        src: '**/*.{png,jpg,jpeg}',
+                        dest: '<%= config.dist %>/images'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    /*removeCommentsFromCDATA: true,
+                     // https://github.com/yeoman/grunt-usemin/issues/44
+                     //collapseWhitespace: true,
+                     collapseBooleanAttributes: true,
+                     removeAttributeQuotes: true,
+                     removeRedundantAttributes: true,
+                     useShortDoctype: true,
+                     removeEmptyAttributes: true,
+                     removeOptionalTags: true*/
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= config.app %>/main/resources/',
+                        src: ['templates/**/*.html', 'templates/**/*.html'],
+                        dest: '<%= config.dist %>'
+                    },
+                    {   expand: true,
+                        cwd: '<%= config.app %>/main/',
+                        src: ['index.html'],
+                        dest: '<%= config.dist %>'
+
+                    }
+                ]
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= config.app %>/main',
+                    dest: '<%= config.dist %>',
+                    src: [
+                        '*.{ico,png,txt}',
+                        'resources/images/{,*/}*.{gif,webp,svg,jpg}',
+                        'js/lib/**/*'
+                    ]
+                }, {
+                    expand: true,
+                    cwd: '.tmp/images',
+                    dest: '<%= config.dist %>/images',
+                    src: [
+                        'generated/*'
+                    ]
+                }]
+            }
+        },
+        concurrent: {
+            dist: [
+                'imagemin',
+                'htmlmin'
+            ]
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        },
+        ngmin: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= config.dist %>/js',
+                        src: '*.js',
+                        dest: '<%= config.dist %>/js'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    '<%= config.dist %>/js/scripts.js': [
+                        '<%= config.dist %>/js/scripts.js'
+                    ]
+                }
+            }
+        },
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%= config.dist %>/js/**/*.js',
+                        '<%= config.dist %>/styles/{,*/}*.css',
+                        '<%= config.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                        '!<%= config.dist %>/js/lib/**/*.js'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('server', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+        }
+        grunt.task.run([
+            'clean:server',
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('test', [
+        'clean:server',
+        'connect:test',
+        'karma'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'useminPrepare',
+        'concurrent:dist',
+        'concat',
+        'copy',
+        'ngmin',
+        'uglify',
+        'rev',
+        'usemin'
+    ]);
+
+    grunt.registerTask('default', [
+        'jshint',
+        'test',
+        'build'
+    ]);
+};
+
---
+++ src/main/js/controllers/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: naresh
+ * Date: 7/17/13
+ * Time: 12:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
